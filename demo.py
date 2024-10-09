66f3bbf3b3421b45107509fbda333ac8fc1548e0
Fabian Valle <ranfys.valle@gmail.com>
2024-10-09 06:30:10 -0400

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,49 @@
+import os
+# Azure OpenAI Setup
+from langchain_openai import AzureChatOpenAI
+AZURE_OPENAI_ENDPOINT = "https://.openai.azure.com"
+AZURE_OPENAI_API_KEY = ""
+deployment_name = ""  # The name of your model deployment
+default_llm = AzureChatOpenAI(
+	openai_api_version=os.environ.get("AZURE_OPENAI_VERSION", "2023-07-01-preview"),
+	azure_deployment=deployment_name,
+	azure_endpoint=AZURE_OPENAI_ENDPOINT,
+	api_key=AZURE_OPENAI_API_KEY
+)
+# File Reader Setup
+from crewai_tools import (
+    FileReadTool,
+)
+# Instantiate tools
+file_tool = FileReadTool(file_path='./injection-00.txt')
+# Research Agent Setup
+from crewai import Crew, Process, Task, Agent
+AGENT_ROLE = "Researcher"
+AGENT_GOAL = """
+This agent summarizes content from some content provided by your file_reader tool into an E-mail in markdown format.
+"""
+researcher = Agent(
+    role=AGENT_ROLE,
+    goal=AGENT_GOAL,
+    verbose=True,
+    llm=default_llm,
+    backstory="",
+    tools=[file_tool],
+)
+# Task Definition
+task1 = Task(
+    description="""
+Summarize content from  some content provided by your file_reader tool into an E-mail in markdown format.
+""",
+    agent=researcher,
+    expected_output="",
+    tools=[file_tool],
+    output_file='output/new_email.md'
+)
+# Crew Creation with Sequential Process
+tech_crew = Crew(
+    agents=[researcher],
+    tasks=[task1],
+    process=Process.sequential,
+)
+tech_crew.kickoff()
